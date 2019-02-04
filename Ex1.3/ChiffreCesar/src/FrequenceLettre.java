c4d88677b67607adbbc56c0cb0626f859ab0b5fe
NDECROIX <nicolas.decroixx@gmail.com>
2019-02-04 19:34:16 +0100

    1.3.2

--- Ex1.3/ChiffreCesar/src/FrequenceLettre.java
+++ Ex1.3/ChiffreCesar/src/FrequenceLettre.java
@@ -9,7 +9,7 @@ public class FrequenceLettre {
      * @param txt texte
      * @return hash map d'occurrence par caractère
      */
-    private Map occurrences(String txt){
+    private Occure[] occurrences(String txt){
 
         Map<Character, Integer> fC = new HashMap<>(); // un hashmap qui va contenir un caractère et son nombre d'occurences
 
@@ -19,23 +19,74 @@ public class FrequenceLettre {
 
             if (c != ' '){
                 fC.merge(c, 1, (a, b) -> a + b);
-                //si le caractère exist pas dans fC on l'ajoute sinon on MAJ sa valeur
+                //si le caractère exist pas dans fC on l'ajoute sinon on MAJ sa valeur à +1
             }
         }
-        return fC;
+
+
+        return trieListe(fC);
     }
 
+    /**
+     * @param fC Map<Character, Integer>
+     * @return tableau trié
+     */
+    private Occure[] trieListe(Map<Character, Integer> fC)
+    {
+        int sizeL = fC.size();
+        Occure[] tblOcc = new Occure[sizeL];
+
+        int t = 0;
+        for (HashMap.Entry<Character, Integer> entry : fC.entrySet())
+        {
+            tblOcc[t] = new Occure(entry.getKey(), entry.getValue()); //
+            t++;
+        }
+
+        Occure transpose;
+
+        for (int i = 0; i < tblOcc.length; i++){
+
+            for (int v = i; v < tblOcc.length; v++){
+
+                if (tblOcc[i].i < tblOcc[v].i){
+                    transpose = tblOcc[i];
+                    tblOcc[i] = tblOcc[v];
+                    tblOcc[v] = transpose;
+                }
+            }
+        }
+
+        return tblOcc;
+
+    }
+    
+    class Occure {
+
+        char a;
+        int i;
+
+        public Occure(char a, int i) {
+            this.a = a;
+            this.i = i;
+        }
+
+    }
 
 
     public static void main(String[] args){
 
         FrequenceLettre fl = new FrequenceLettre();
 
-        System.out.println(fl.occurrences("You shall not passsss !"));
-
+        for (Occure oc : fl.occurrences("You shall not passsss !")){
+            System.out.print(oc.i + " " + oc.a + "     " );
+        }
 
     }
 
 }
+
+
+
 
 
