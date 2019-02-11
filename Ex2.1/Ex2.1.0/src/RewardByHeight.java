84793cc5444383f0f0762c4e27e0c8dabb87bcd9
NDECROIX <nicolas.decroixx@gmail.com>
2019-02-11 15:22:14 +0100

    Doc RewardByHeight

--- Ex2.1/Ex2.1.0/src/RewardByHeight.java
+++ Ex2.1/Ex2.1.0/src/RewardByHeight.java
@@ -2,9 +2,9 @@ import java.text.DecimalFormat;
 
 public class RewardByHeight {
 
-    private static final int CHANGE_REWARD = 210000;
-    private static final int REWARD_DIVISOR = 2;
-    private static final float FIRST_REWARD = 50;
+    private static final int CHANGE_REWARD = 210000; // nombre de bloc avant de réduire la récompense
+    private static final int REWARD_DIVISOR = 2; // nombre par lequel l'on divise la récompense tous les 210000 blocs
+    private static final float FIRST_REWARD = 50; // Récompense de base pour la génération d'un bloc
 
 
     /**
@@ -14,7 +14,7 @@ public class RewardByHeight {
     public String rewardByHeight(int blockHeight){
 
 
-        int x = (blockHeight / CHANGE_REWARD); // on divise le block par 210000 : nombre de blocs qui réajuste la récompense
+        int x = (blockHeight / CHANGE_REWARD); // on divise le block par 210000
 
         int level = (int) Math.pow(REWARD_DIVISOR, x); // l'on fait 2 puissance x
 
@@ -27,7 +27,7 @@ public class RewardByHeight {
         RewardByHeight rBH = new RewardByHeight();
 
 
-        System.out.println(rBH.rewardByHeight(1));
+        System.out.println(rBH.rewardByHeight(0));
         System.out.println(rBH.rewardByHeight(210000));
         System.out.println(rBH.rewardByHeight(420000));
         System.out.println(rBH.rewardByHeight(630000));
