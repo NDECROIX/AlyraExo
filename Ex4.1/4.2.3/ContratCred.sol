b567c4f4397331256f1b2c094ad344f0ddde1054
NDECROIX <nicolas.decroixx@gmail.com>
2019-02-25 22:07:43 +0100

    Exo4.2.3 contratCred

---
+++ Ex4.1/4.2.3/ContratCred.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.5.2;
+
+
+import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+
+contract Credibilite {
+
+
+   using SafeMath for uint256;
+
+   mapping (address => uint256) public cred;
+
+   bytes32[] private devoirs;
+   
+   uint private addCred;
+   
+   event RendreDevoir(bytes32 hashDevoir, address adr);
+   
+   constructor() public {
+       addCred = 30;
+   }
+   
+   
+   function produireHash(string memory url) public pure returns (bytes32){
+       bytes32 urlB;
+       
+       assembly { urlB := mload(add(url, 32)) }
+       
+       return keccak256(abi.encodePacked(urlB));
+       
+   }
+   
+   function transfer(address destinataire, uint256 valeur) public {
+       require(cred[msg.sender] > (valeur)); // il doit avoir un cred de plus que la valeur.
+       require(cred[destinataire] != 0);
+       cred[msg.sender] -= valeur;
+       cred[destinataire] += valeur;
+   }
+   
+   function remettre(bytes32 dev) public returns (uint){
+       devoirs.push(dev);
+       cred[msg.sender] += addCred;
+       if(addCred > 10) addCred -= 10;
+       
+       emit RendreDevoir(dev, msg.sender);
+       return devoirs.length;
+       
+   }
+   
+}
+
+
